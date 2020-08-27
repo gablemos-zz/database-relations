5db015e75d8acb25d1afb583910767d7e002d003
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2020-08-27 17:04:29 -0300

    codigo alterado

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -6,8 +6,18 @@ import CreateProductService from '@modules/products/services/CreateProductServic
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, price, quantity } = request.body;
+
     const createProduct = container.resolve(CreateProductService);
+
     const product = await createProduct.execute({ name, price, quantity });
+
+    if (quantity !== 0) {
+      console.log(name);
+    }
+
+    if (quantity !== 0) {
+      console.log(name);
+    }
 
     return response.json(product);
   }
